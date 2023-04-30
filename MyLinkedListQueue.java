060559da764b033e2c0a7608aeee0b10bdf5c544
Aldiyar <aldiyarmaratov0@gmail.com>
2023-04-30 23:55:30 +0600

    added comments into the queue

--- MyLinkedListQueue.java
+++ MyLinkedListQueue.java
@@ -1,22 +1,66 @@
 import java.util.NoSuchElementException;
-public class MyLinkedListQueue<T> extends MyArrayList<T>{
-    MyArrayList list;
-    public MyLinkedListQueue(){
-        this.list=new MyArrayList();
+
+/**
+ * A generic implementation of a queue using ArrayList.
+ * @param <T> the type of elements held in this queue
+ */
+public class MyLinkedListQueue<T> extends MyArrayList<T> {
+    MyArrayList<T> list;
+
+    /**
+     * Constructs an empty queue.
+     */
+    public MyLinkedListQueue() {
+        this.list = new MyArrayList<>();
     }
-    public void enqueue(T item){
-        list.add(item,list.size()-1);
+
+    /**
+     * Inserts the specified element into this queue.
+     * @param item the element to add
+     */
+    public void enqueue(T item) {
+        list.add(item, list.size() - 1);
     }
-    public T dequeue(){
-        return (T) list.remove(0);
+
+    /**
+     * Retrieves and removes the head of this queue.
+     * @return the head of this queue
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public T dequeue() throws NoSuchElementException {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return (T) list.remove(0);
+        }
     }
-    public T peek(){
-        return (T) list.get(0);
+
+    /**
+     * Retrieves, but does not remove, the head of this queue.
+     * @return the head of this queue
+     * @throws NoSuchElementException if this queue is empty
+     */
+    public T peek() throws NoSuchElementException {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return (T) list.get(0);
+        }
     }
-    public boolean isEmpty(){
+
+    /**
+     * Returns true if this queue contains no elements.
+     * @return true if this queue contains no elements, false otherwise
+     */
+    public boolean isEmpty() {
         return list.isEmpty();
     }
-    public int size(){
+
+    /**
+     * Returns the number of elements in this queue.
+     * @return the number of elements in this queue
+     */
+    public int size() {
         return list.size();
     }
 }
